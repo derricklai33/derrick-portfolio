304a2fe860c2326b68094fcb304886de903db5ff
Derrick Lai <derricklai33@hotmail.com>
2020-09-03 14:29:13 +1000

    Completed implementation of mobile navbar (with basic javascript for onclick effect) and basic template for browser version,

---
+++ src/style/javascript/animation.js
@@ -0,0 +1,10 @@
+const navSilde = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click',() => {
+        nav.classList.toggle('nav-active');
+    });
+}
+
+navSilde();
