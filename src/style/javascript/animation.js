6766df31b1f598e40e54c033b27f68202e8385d6
Derrick Lai <derricklai33@hotmail.com>
2020-09-04 22:08:59 +1000

    Completed mobile version for mainpage. Made changes for layout.scss for responsiveness.

--- src/style/javascript/animation.js
+++ src/style/javascript/animation.js
@@ -1,16 +1,18 @@
 const navSilde = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navbar2 = document.querySelector('.navbar');
 
     burger.addEventListener('click',() => {
         nav.classList.toggle('nav-active');
+        navbar2.classList.toggle('nav-active2');
     });
 }
 
 const navbarScroll = () => {
 
     const navbar = document.querySelector('.navbar');
-    window.addEventListener('scroll', function () {
+    window.addEventListener('scroll', function () {    
         if (window.innerWidth > 800) {
             navbar.classList.toggle('scrolling-active', window.scrollY > 0);
         }
